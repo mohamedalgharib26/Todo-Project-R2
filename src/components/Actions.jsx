6660abc9783ddbcbb8f42fc1b9acd10b0a80856b
Mohamed Algharib <mohamedalgharib26@gmail.com>
2025-05-18 21:03:39 +0300

    edit the actions

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,10 +1,13 @@
-function Actions() {
+function Actions({ deleteTodo, id }) {
   return (
     <div>
       <button className="border-0 btn-transition btn btn-outline-success">
         <i className="fa fa-pen"></i>
       </button>
-      <button className="border-0 btn-transition btn btn-outline-danger">
+      <button
+        onClick={() => deleteTodo(id)}
+        className="border-0 btn-transition btn btn-outline-danger"
+      >
         <i className="fa fa-trash"></i>
       </button>
     </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,25 +3,47 @@ import Actions from "./Actions";
 
 function Todo() {
   const [todos, setTodos] = useState([]);
+  //read
   const FetchData = async () => {
     const response = await fetch("http://localhost:3000/Todos");
     const data = await response.json();
     setTodos(data);
   };
+  //Delete
+  const DeleteTodo = async (id) => {
+    const response = await fetch(`http://localhost:3000/Todos/${id}`, {
+      method: "delete",
+    });
+    const data = await response.json();
+    FetchData();
+    console.log(data);
+  };
+  //create -- Add
+
   useEffect(() => {
     FetchData();
   }, []);
+
   return (
     <div>
-      {todos.map((todo, index) => (
-        <div className="">
+      {todos.map((todo) => (
+        <div key={todo.id}>
           <div className="widget-content-left">
-            <h5 key={index} className="widget-heading">
-              {todo.title}
+            <h5 className="widget-heading">
+              {todo.id} - {todo.title}
             </h5>
           </div>
           <div className="widget-content-right">
-            <Actions />
+            {/* <Actions deleteTodo={DeleteTodo} id={todo.id} /> */}
+            <button className="border-0 btn-transition btn btn-outline-success">
+              <i className="fa fa-pen"></i>
+            </button>
+            <button
+              onClick={() => DeleteTodo(todo.id)}
+              className="border-0 btn-transition btn btn-outline-danger"
+            >
+              <i className="fa fa-trash"></i>
+            </button>
           </div>
         </div>
       ))}
